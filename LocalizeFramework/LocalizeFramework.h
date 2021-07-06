0bcb7855a6c4128bc244b4d5987e7f0025d435e4
Rakib <rakib@fastsolutioninc.com>
2021-07-06 17:39:46 +0600

    Initial Commit

---
+++ LocalizeFramework/LocalizeFramework.h
@@ -0,0 +1,18 @@
+//
+//  LocalizeFramework.h
+//  LocalizeFramework
+//
+//  Created by Newroz Tech on 6/7/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LocalizeFramework.
+FOUNDATION_EXPORT double LocalizeFrameworkVersionNumber;
+
+//! Project version string for LocalizeFramework.
+FOUNDATION_EXPORT const unsigned char LocalizeFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LocalizeFramework/PublicHeader.h>
+
+
